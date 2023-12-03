c7c8a5a314539983db302f6cfb0031e3edba9879
KouichiTsushima <147828512+TsuKou@users.noreply.github.com>
2023-12-03 06:22:00 +0000

    休憩

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,23 @@
 class RelationshipsController < ApplicationController
   def create
+    @user = User.find(params[:user_id])
+    current_user.follow(params[:user_id])
+    @user.create_notification_follow!(current_user)
+    redirect_to request.referer
   end
 
   def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
+  end
+
+  def yes_follow
+    user = User.find(params{:user_id})
+    @users = user.yes_follow
+  end
+
+  def no_follow
+    user = User.find(params[:user_id])
+    @users = user.no_follow
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,18 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
 
+  get "/home/about"=>"homes#about", as: "about"
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorite, only: [:create, :destroy]   #応用課題3で「do ~ end」まで追加
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relation, only: [:create, :destroy]
+    get "yes_follow" => "relationships#yes_follow", as: "yes_follow"
+    get "no_follow" => "relatioships#no_follow", as: "no_follow"
+  end
 
-  get "/home/about"=>"homes#about", as: "about"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
