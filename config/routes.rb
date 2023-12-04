dfdbea2eda489c153784e82ac7d0c3a51a9f3849
KouichiTsushima <147828512+TsuKou@users.noreply.github.com>
2023-12-04 08:21:13 +0000

    [Finish]2023/12/03

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
     resource :favorite, only: [:create, :destroy]   #応用課題3で「do ~ end」まで追加
   end
   resources :users, only: [:index,:show,:edit,:update] do
-    resource :relationships, only: [:create, :destroy]
+    resources :relationships, only: [:create, :destroy]
     get "followings" => "relationships#followings", as: "followings"
-    get "followers" => "relatioships#followers", as: "followers"
+    get "followers" => "relationships#followers", as: "followers"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
