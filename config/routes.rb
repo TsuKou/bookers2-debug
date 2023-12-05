51744ee25766b4b1570a2357758c35b546c18aca
KouichiTsushima <147828512+TsuKou@users.noreply.github.com>
2023-12-05 11:13:07 +0000

    [Finish]2023/12/05

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get "/home/about"=>"homes#about", as: "about"
+  get "search" => "searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
